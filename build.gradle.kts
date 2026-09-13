f86eec06930d1b5d8a2900fcf5da85d338959a48
moddedmodD <267982042+moddedmodD@users.noreply.github.com>
2026-09-13 10:09:36 +0200

    Removed Fabric api because I don't need it anymore

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
 	minecraft("com.mojang:minecraft:${providers.gradleProperty("minecraft_version").get()}")
 	implementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
 
-	implementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
+	//implementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
 }
 
 tasks.processResources {
